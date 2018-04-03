d0099df7d03b289195bc219789264f0f91d20990
GennadyZaskokin <38049752+GennadyZaskokin@users.noreply.github.com>
2018-04-03 23:00:25 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,51 @@
-const bott = new Discord.Client();
-const prefix = '!';
+// Load up the discord.js library
+const Discord = require("discord.js");
 
-bott.on('message', message => {
-if(message.author.bot) return;
-        if(message.content.indexOf(prefix) !== 0) return;
-        const args = message.content.slice(prefix.length).trim().split(/ +/g);
-        const command = args.shift().toLowerCase();
-if (command === 'clear') {
-  if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply ("Ошибка"); 
-  if(!args[0]) return message.channel.send ("Ошибка"); 
-  message.channel.bulkDelete(args[0]+1).then(() => { 
-  message.channel.send('Deleted ${args[0]} messages.').then(msg =>msg.delete(5000)); 
-  }); 
-}
+// This is your client. Some people call it `bot`, some people call it `self`, 
+// some might call it `cootchie`. Either way, when you see `client.something`, or `bot.something`,
+// this is what we're refering to. Your client.
+const client = new Discord.Client();
+
+// Here we load the config.json file that contains our token and our prefix values. 
+const config = require("./config.json");
+// config.token contains the bot's token
+// config.prefix contains the message prefix.
+
+client.on("ready", () => {
+  // This event will run if the bot starts, and logs in, successfully.
+  console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
+  // Example of changing the bot's playing game to something useful. `client.user` is what the
+  // docs refer to as the "ClientUser".
+  //client.user.setGame(`on ${client.guilds.size} servers`);
+});
+
+
+
+client.on("message", async message => {
+  // This event will run on every single message received, from any channel or DM.
+  
+  // It's good practice to ignore other bots. This also makes your bot ignore itself
+  // and not get into a spam loop (we call that "botception").
+  if(message.author.bot) return;
+  
+  // Also good practice to ignore any message that does not start with our prefix, 
+  // which is set in the configuration file.
+  if(message.content.indexOf(config.prefix) !== 0) return;
+  
+  // Here we separate our "command" name, and our "arguments" for the command. 
+  // e.g. if we have the message "+say Is this the real life?" , we'll get the following:
+  // command = say
+  // args = ["Is", "this", "the", "real", "life?"]
+  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+  
+  
+  
+  if(command === "clear") {
+
+    message.channel.bulkDelete (100)
+   
+  }
 });
 
 client.login(process.env.BOT_TOKEN);
